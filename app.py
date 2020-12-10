2fbd1c3075f0693f2b33ef0dc5e279d6a1b265ab
phattran <phat.tranhoang@cyberlogitec.com>
2020-12-10 21:17:18 +0700

    cccc

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import (Flask, render_template, make_response, request, send_file)
+from flask_cors import CORS, cross_origin
+import json
+import os
+import sys
+import recommender
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+def response_json(key, val):
+    return make_response(json.dumps({
+        key: val
+    }))
+
+@app.route("/")
+def index():
+    return response_json('msg', 'Hi there, what are you looking for?')
+
+@app.route("/recommend-by-video/<video_id>")
+def recommend_for_video(video_id=-1):
+    if video_id == -1:
+        return response_json("msg", "Please provide video id")
+
+    videos = recommender.recommend_for_video(video_id)
+
+    return response_json(videos)
+
+
+@app.route("/recommend-by-user/<user_id>")
+def recommend_for_user(user_id=-1):
+    if user_id == -1:
+        return response_json("msg", "Please provide video id")
+
+    videos = recommender.recommend_for_user(user_id)
+
+    return response_json(videos)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    debug = True
+    if not debug:
+        app.run(threaded=True, host='0.0.0.0', port=port)
+    else:
+        app.run(debug=True, threaded=True)
---
+++ recommender.py
@@ -0,0 +1,13 @@
+
+
+## Export ##
+def recommend_for_video(video_id):
+    
+    
+    return videos
+
+
+def recommend_for_user(user_id):
+    
+    
+    return videos
