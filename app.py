b6960a0e20f622755e513ac1fe98665f90cb57e9
phattran <phatth1203@gmail.com>
2020-12-11 00:49:10 +0700

    ko

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import (Flask, render_template, make_response, request, send_file)
+from flask import Flask, render_template, make_response, request, send_file
 from flask_cors import CORS, cross_origin
+from flaskext.mysql import MySQL
 import json
 import os
 import sys
 import recommender
+import pandas as pd
+
+mysql = MySQL()
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'vitube'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
 
 def response_json(key, val):
     return make_response(json.dumps({
@@ -17,14 +27,14 @@ def response_json(key, val):
 
 @app.route("/")
 def index():
-    return response_json('msg', 'Hi there, what are you looking for?')
+    return response_json('msg', 1)
 
 @app.route("/recommend-by-video/<video_id>")
 def recommend_for_video(video_id=-1):
     if video_id == -1:
         return response_json("msg", "Please provide video id")
 
-    videos = recommender.recommend_for_video(video_id)
+    videos = recommender.recommend_for_video(video_id, cursor)
 
     return response_json(videos)
 
@@ -34,7 +44,7 @@ def recommend_for_user(user_id=-1):
     if user_id == -1:
         return response_json("msg", "Please provide video id")
 
-    videos = recommender.recommend_for_user(user_id)
+    videos = recommender.recommend_for_user(user_id, cursor)
 
     return response_json(videos)
 
--- recommender.py
+++ recommender.py
@@ -1,13 +1,56 @@
+import os
+import pandas as pd
+import numpy as np
+import scipy
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+class Recommender:
+    def __init__(self, mysql):
+        self.mysql = mysql
+
+    def connect(self):
+        connection = self.mysql.connect()
+        self.cursor = connection.cursor()
+
+    @staticmethod
+    def save_all_vectors(vectors):
+        np.save("all_vectors", vectors)
+
+    def load_vectors(self):
+        self.vectors = np.load("all_vectors.npy")
+
+    def init_all_dataframe(self):
+        self.activity_df = self.build_df("activities", "id,type,user_id,target_id")
+        self.history_df = self.build_df(
+            "watch_histories",
+            "id,user_id,video_id,current_like,current_dislike,current_view"
+        )
+        self.video_df = self.build_df(
+            "videos",
+            "id,video_src,thumbnail,name,description,duration,tags"
+        )
+
+    def build_df(self, table, columns, condition=""):
+        sql = f"SELECT {columns} from {table}"
+        if condition:
+            sql += f"WHERE ${condition}"
+
+        self.cursor.execute(sql)
+        data = self.cursor.fetchall()
+        df = pd.DataFrame(data)
+        df.columns = columns.split(",")
+        return df
+    
 
 ## Export ##
-def recommend_for_video(video_id):
+def recommend_for_video(video_id, cursor):
     
     
     return videos
 
 
-def recommend_for_user(user_id):
+def recommend_for_user(user_id, cursor):
     
     
     return videos
